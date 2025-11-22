9326c7f9bc3e4f9445ce64b20b6667e49c176136
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2025-11-22 23:41:26 +0530

    update the Deposit page

--- src/main/java/com/Bank/Gui/Deposit.java
+++ src/main/java/com/Bank/Gui/Deposit.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Date;
+import com.Bank.Dao.Bankdao;
 
 public class Deposit{
     
@@ -11,33 +12,54 @@ public class Deposit{
     JButton depositBtn, backBtn;
     String pin;
     JFrame frame;
+    Bankdao bankdao;
 
     Deposit(String pin){
         this.pin = pin;
-        frame=new JFrame("Deposit");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+        initialsteps();
+        addListeners();
+    }
+    void initialsteps(){
+        bankdao = new Bankdao();
+        frame = new JFrame("Deposit Page");
+        frame.setSize(400,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
-        
-        JLabel text = new JLabel("ENTER AMOUNT TO DEPOSIT");
-        text.setFont(new Font("System", Font.BOLD, 16));
-        text.setBounds(170, 30, 400, 20);
-        frame.add(text);
+        frame.setLocationRelativeTo(null);
+
+        JLabel amountLabel = new JLabel("Enter Amount to Deposit:");
+        amountLabel.setBounds(50,50,200,30);
+        frame.add(amountLabel);
 
         amountField = new JTextField();
-        amountField.setFont(new Font("Raleway", Font.BOLD, 22));
-        amountField.setBounds(170, 65, 320, 25);
+        amountField.setBounds(200,50,100,30);
         frame.add(amountField);
 
-        depositBtn = new JButton("DEPOSIT");
-        depositBtn.setBounds(355, 485, 150, 30);
+        depositBtn = new JButton("Deposit");
+        depositBtn.setBounds(50,100,100,30);
         frame.add(depositBtn);
 
-        backBtn = new JButton("BACK");
-        backBtn.setBounds(355, 520, 150, 30);
+        backBtn = new JButton("Back");
+        backBtn.setBounds(200,100,100,30);
         frame.add(backBtn);
 
-        frame.setSize(900, 900);
-        frame.setLocation(300, 0);
         frame.setVisible(true);
+    }
+
+    void addListeners(){
+        depositBtn.addActionListener(e->{
+            String amountText=amountField.getText();
+            Date date = new Date();
+            try{
+                int row=bankdao.deposit(pin,amountText,date);
+                if(row>0){
+                    JOptionPane.showMessageDialog(frame, "Amount Deposited Successfully");
+                    frame.dispose();
+                    Home homePage=new Home();
+                }
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(frame, "Error in depositing amount","Error",JOptionPane.ERROR_MESSAGE);
+            }
+        });
     }
 }
