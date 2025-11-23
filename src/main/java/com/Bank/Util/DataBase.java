a076073d970eb1139d3af53390fd5edfd03f3a22
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2025-11-23 16:16:55 +0530

    Database schema changed so this page changed

--- src/main/java/com/Bank/Util/DataBase.java
+++ src/main/java/com/Bank/Util/DataBase.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBase {
-    private static final String URL = "jdbc:mysql://localhost:3306/todo";
+    private static final String URL = "jdbc:mysql://localhost:3306/Bank_system";
     private static final String USER = "root";
     private static final String PASSWORD = "vishnu";
 
