82afd570996266a53463ddf54587f9e017eef6fa
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2025-11-23 23:18:31 +0530

    update error correction and Balance Logic

--- src/main/java/com/Bank/Gui/Balance.java
+++ src/main/java/com/Bank/Gui/Balance.java
@@ -1,12 +1,14 @@
 package com.Bank.Gui;
 import javax.swing.*;
 import java.awt.*;
-
+import com.Bank.Dao.Bankdao;
 public class Balance{
     JFrame frame;
     JLabel balanceLabel;
     JButton backButton;
     String userAccount,userpin;
+    Bankdao bankdao;
+    int balance;
     public Balance(String userAccount,String userpin){
         this.userAccount=userAccount;
         this.userpin=userpin;
@@ -14,12 +16,19 @@ public class Balance{
         addListeners();
     }
     void setup(){
+        bankdao=new Bankdao();
         frame=new JFrame("Balance Page");
         frame.setSize(400,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setLocationRelativeTo(null);
-        balanceLabel=new JLabel("Your balance is: $1000");
+        try{
+        balance=bankdao.getBalance(userAccount);
+        balanceLabel=new JLabel();
+        balanceLabel.setText("Your Balance is: " + balance);
+        }catch(Exception e){
+            balanceLabel=new JLabel("Error retrieving balance Retry later");
+        }
         balanceLabel.setBounds(130,80,200,30);
         frame.add(balanceLabel);
         backButton=new JButton("Back");
@@ -31,7 +40,6 @@ public class Balance{
         backButton.addActionListener(e -> {
             frame.dispose();
             Home homePage=new Home(userAccount,userpin);
-        });
-        
+        });        
     }    
 }
